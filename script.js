5f2c504ba5c87d28e42777463d6880bde1ec7886
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-09-28 09:57:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function dadosformulario(){
+    txt= document.getElementById("txt1");
+    sel=document.getElementById("sel");
+    box=document.getElementsByName ("box") [0];
+    radio=document.getElementsByName("radio") [0];
+    console.log(box);
+}
